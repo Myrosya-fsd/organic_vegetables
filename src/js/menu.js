0ed6beef309b158dece1454e9b00c2549534b703
myroslava <murosja80@gmail.com>
2025-06-12 15:35:18 +0300

    update

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,18 +1,22 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
+    menuLinks: document.querySelectorAll('.mob-menu-a'), // Додаємо всі посилання меню
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  // Додаємо закриття меню при кліку на будь-яке посилання
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.modal.classList.remove('is-open');
+    });
+  });
+
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
   }
 })();
